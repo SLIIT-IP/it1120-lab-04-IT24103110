711a5ee0e717e76f088bf764918484f1d829865d
IT24103110 <it24103110@my.sliit.lk>
2024-08-16 10:16:12 +0530

    IT24103110Lab4Q1.java

---
+++ Q1/IT24103110Lab4Q1.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class IT24103110Lab4Q1 {
+    public static void main(String[]args){
+     int number;
+     Scanner input= new Scanner(System.in);
+     System.out.println("Enter a number: ");
+     number=input.nextInt();
+  if(number>0)
+  {
+  System.out.println("The number is positive");
+  }
+  else if(number<0)
+  {
+  System.out.println("The number is negative");
+  }
+  else
+  {
+  System.out.println("The number is zero");
+  }
+ }
+}
